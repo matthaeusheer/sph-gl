8b5ee008cfa84bce89e26bd1cb98f30a5333d98a
Matthäus Heer <mathee@physik.uzh.ch>
2017-11-06 15:45:38 +0100

    Cleaning up and split into header/source

---
+++ GasoViz/src/graphics/Shader.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <GLAD/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <string>
+
+class Shader
+{
+private:
+	unsigned int m_ID; 
+	const char* m_vertexPath;
+	const char* m_fragmentPath;
+
+public:
+	Shader() {
+	}
+	
+	Shader(const char* vertexPath, const char* fragmentPath);
+	~Shader();
+
+	void enable() const;
+	void disable() const;
+
+	// utility uniform functions
+	// ------------------------------------------------------------------------
+	GLint getUniformLocation(const GLchar* name);
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string &name, int value) const;
+	void setFloat(const std::string &name, float value) const;
+	void setVec2(const std::string &name, const glm::vec2 &value) const;
+	void setVec2(const std::string &name, float x, float y) const;
+	void setVec3(const std::string &name, const glm::vec3 &value) const;
+	void setVec3(const std::string &name, float x, float y, float z) const;
+	void setVec4(const std::string &name, const glm::vec4 &value) const;
+	void setVec4(const std::string &name, float x, float y, float z, float w);
+	void setMat2(const std::string &name, const glm::mat2 &mat) const;
+	void setMat3(const std::string &name, const glm::mat3 &mat) const;
+	void setMat4(const std::string &name, const glm::mat4 &mat) const;
+
+private:
+	GLuint load();
+	void checkCompileErrors(unsigned int shader, std::string type);
+
+};
